bca7242c9710f81d7dc5e4c99e8b452953eb8d64
Danijel Jovanović <danijel.xda@gmail.com>
2024-05-02 21:54:47 +0200

    Update op

--- GSP_RVA/Service/Database/CRUDOperations/VozacCrud/UpdateVozac.cs
+++ GSP_RVA/Service/Database/CRUDOperations/VozacCrud/UpdateVozac.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Service.Database.Context;
+using Service.Database.CRUD;
+using Service.Database.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,29 @@ using System.Threading.Tasks;
 
 namespace Service.Database.CRUDOperations.VozacCrud
 {
-    internal class UpdateVozac
+    public class UpdateVozac : IUpdateOperation<Vozac>
     {
+        private readonly DatabaseContext _context;
+        private static readonly object _lock = new object();
+
+        public UpdateVozac(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public bool Update(int id, Vozac vozac)
+        {
+            // Use a lock to ensure thread safety
+            lock (_lock)
+            {
+                Vozac find = _context.Vozaci.Find(id);
+
+                if (find == null)
+                    return false;
+
+                find = vozac;
+                return _context.SaveChanges() > 0;
+            }
+        }
     }
 }
