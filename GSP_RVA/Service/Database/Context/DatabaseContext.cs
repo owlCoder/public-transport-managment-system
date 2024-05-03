cd1c35b89b93cc448807fa09ff89aa250410733d
Danijel Jovanović <danijel.xda@gmail.com>
2024-05-03 09:21:15 +0200

    Update docs

--- GSP_RVA/Service/Database/Context/DatabaseContext.cs
+++ GSP_RVA/Service/Database/Context/DatabaseContext.cs
@@ -4,17 +4,30 @@ using System.Data.Entity;
 
 namespace Service.Database.Context
 {
+    /// <summary>
+    /// Represents the database context for interacting with the MySQL database using Entity Framework.
+    /// </summary>
     [DbConfigurationType(typeof(MySqlEFConfiguration))]
-
     public class DatabaseContext : DbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatabaseContext"/> class.
+        /// </summary>
         public DatabaseContext() : base("MySqlConnection") { }
 
-        // tabele u bazi
+        /// <summary>
+        /// Gets or sets the <see cref="DbSet{Vozac}"/> for the Vozac entity set (table).
+        /// </summary>
         public DbSet<Vozac> Vozaci { get; set; }
 
+        /// <summary>
+        /// Gets or sets the <see cref="DbSet{Linija}"/> for the Linija entity set (table).
+        /// </summary>
         public DbSet<Linija> Linije { get; set; }
 
-        /// busevi 
+        /// <summary>
+        /// Busevi
+        /// </summary>
+        // TODO: add more tables
     }
 }
