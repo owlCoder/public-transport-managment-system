b4b500b07e4f468d86141e22dba6bb55c04b05c6
jovanaOrestijevic <jovanaorestijevic02@gmail.com>
2024-05-22 19:15:30 +0200

    adding new windows

---
+++ GSP_RVA/Client/ViewModel/AddEditAutobusViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Client.Commands.LinijaCommands;
+using Client.Commands;
+using Client.Provider;
+using Common.DTO;
+using Common.Interfaces;
+using MVVMLight.Messaging;
+using NetworkService.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Commands.Manager;
+
+namespace Client.ViewModel
+{
+    public class AddEditAutobusViewModel : BindableBase
+    {
+
+    }
+}
