ddf48a41e5e06ce2e2a769821f35841b8a03c942
jovanaOrestijevic <jovanaorestijevic02@gmail.com>
2024-05-27 20:50:04 +0200

    edit profile view

--- GSP_RVA/Client/ViewModel/EditProfileViewModel.cs
+++ GSP_RVA/Client/ViewModel/EditProfileViewModel.cs
@@ -1,10 +1,68 @@
-﻿using NetworkService.Helpers;
+﻿using MVVMLight.Messaging;
+using NetworkService.Helpers;
 
 namespace Client.ViewModel
 {
     public class EditProfileViewModel : BindableBase
     {
+        public string Mode { get; private set; }
+        public bool IsSaved { get; private set; }
 
-        public EditProfileViewModel() { }
+        public MyICommand SaveCommand { get; private set; }
+        public MyICommand CancelCommand { get; private set; }
+
+        public string ime;
+        public string prezime;
+
+        public EditProfileViewModel() 
+        {
+            SaveCommand = new MyICommand(OnSave);
+            CancelCommand = new MyICommand(OnCancel);
+        }
+
+        public void OnSave()
+        {
+
+        }
+
+        public void OnCancel()
+        {
+            Messenger.Default.Send(("gsp", ""));
+
+        }
+
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+
+            set
+            {
+                if (ime != value)
+                {
+                    ime = value;
+                    OnPropertyChanged("Ime");
+                }
+            }
+        }
+
+        public string Prezime
+        {
+            get
+            {
+                return prezime;
+            }
+
+            set
+            {
+                if (prezime != value)
+                {
+                    prezime = value;
+                    OnPropertyChanged("Prezime");
+                }
+            }
+        }
     }
 }
